5c8bc8a6e07af531018d0630da3b93e118fee898
Your Name <cjangelo.cj@gmail.com>
2024-09-04 18:42:06 +0800

    fix main tag for homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -68,51 +68,13 @@ const Homepage: React.FC = () => {
       <div className="px-4 pb-4 mx-auto max-w-screen-xl flex flex-col md:flex-row gap-32">
         {/* Main Content Section */}
         <div className="flex-1">
-          <div className="bg-gray-50 border border-gray-200 rounded-lg p-8 md:p-12 mb-8">
-            <a
-              href="#"
-              className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md mb-2"
-            >
-              <svg
-                className="w-2.5 h-2.5 me-1.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 14"
-              >
-                <path d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z" />
-              </svg>
-              Tutorial
-            </a>
-            <h1 className="text-gray-900 text-3xl md:text-5xl font-extrabold mb-2">
-              How to quickly deploy a static website
-            </h1>
-            <p className="text-lg font-normal text-gray-500 mb-6">
-              Static websites are now used to bootstrap lots of websites and are
-              becoming the basis for a variety of tools that even influence both
-              web designers and developers.
+          <div className="bg-gray-50 border border-gray-200 rounded-lg p-8 md:p-6 mb-8">
+          <h1 className="text-gray-900 text-3xl md:text-5xl font-extrabold mb-2 text-center">  
+              Welcome to Tabing Kanto Singapore!  
+          </h1>
+            <p className="text-lg font-normal text-gray-500 mb-6 text-center">
+            Your Essential Hub for Filipinos in Singapore!"
             </p>
-            <a
-              href="#"
-              className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
-            >
-              Read more
-              <svg
-                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
           </div>
           <div
             id="carousel"
