b0a94436f84003ad339eeb237efb91847030e2f3
Your Name <cjangelo.cj@gmail.com>
2024-09-05 17:50:33 +0800

    push change images h and w not final

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import moneyImage from '../images/money.jpg';
+import churchImage from '../images/church.jpg';
+import groceryImage from '../images/grocery.jpg';
+import restoImage from '../images/resto.jpg';
 
 const Gallery: React.FC = () => {
   return (
@@ -6,30 +10,30 @@ const Gallery: React.FC = () => {
       <h2 className="text-white text-2xl font-extrabold mb-4">Quick Links</h2>
       <div className="grid grid-cols-2 gap-2">
         <div className="flex items-center justify-center">
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-            alt="Gallery Image 1"
-          />
+          <img  
+            className="w-40 h-40 object-cover rounded-lg"  
+            src={moneyImage}  
+            alt="Gallery Image 1"  
+          /> 
+        </div>
+        <div className="flex items-center justify-center">
+          <img  
+            className="w-40 h-40 object-cover rounded-lg"  
+            src={churchImage}  
+            alt="Gallery Image 2"  
+          />  
         </div>
         <div className="flex items-center justify-center">
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-            alt="Gallery Image 2"
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+            className="w-40 h-40 object-cover rounded-lg"
+            src={groceryImage} 
             alt="Gallery Image 3"
           />
         </div>
         <div className="flex items-center justify-center">
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+            className="w-40 h-40 object-cover rounded-lg"
+            src={restoImage} 
             alt="Gallery Image 4"
           />
         </div>
