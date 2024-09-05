9e9792a70384c9ec9f67f050d0c935f9b780536d
Your Name <cjangelo.cj@gmail.com>
2024-09-05 18:59:21 +0800

    gallery mouse hover and pointer effect

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -9,30 +9,34 @@ const Gallery: React.FC = () => {
     <div className="w-full h-fit bg-[#7c3732] rounded-lg p-4 md:p-8 gap-4">
       <h2 className="text-white text-2xl font-extrabold mb-4">Quick Links</h2>
       <div className="grid grid-cols-2 gap-2">
+        {/* Card 1 */}
         <div className="flex items-center justify-center">
           <img  
-            className="w-40 h-40 object-cover rounded-lg"  
+            className="w-80 h-80 object-cover rounded-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105"  
             src={moneyImage}  
             alt="Gallery Image 1"  
           /> 
         </div>
+        {/* Card 2 */}
         <div className="flex items-center justify-center">
           <img  
-            className="w-40 h-40 object-cover rounded-lg"  
+            className="w-80 h-80 object-cover rounded-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105"  
             src={churchImage}  
             alt="Gallery Image 2"  
           />  
         </div>
+        {/* Card 3 */}
         <div className="flex items-center justify-center">
           <img
-            className="w-40 h-40 object-cover rounded-lg"
+            className="w-80 h-80 object-cover rounded-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105"
             src={groceryImage} 
             alt="Gallery Image 3"
           />
         </div>
+        {/* Card 4 */}
         <div className="flex items-center justify-center">
           <img
-            className="w-40 h-40 object-cover rounded-lg"
+            className="w-80 h-80 object-cover rounded-lg cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105"
             src={restoImage} 
             alt="Gallery Image 4"
           />
